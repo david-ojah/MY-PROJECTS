0df7e0c97065abb8c59d6bc20f8c4045189874fa
david-ojah <109018190+david-ojah@users.noreply.github.com>
2022-07-13 18:52:47 +0530

    Update

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from classes.game import Person, Bcolors
 
-magic = [{"name": "Fire", "cost": 10, "damage": 30},
-         {"name": "Ice", "cost": 20, "damage": 40},
-         {"name": "Water", "cost": 5, "damage": 20}]
-player = Person(500, 65, 34, 60, magic)
+magic = [{"name": "Fire Magic", "cost": 10, "damage": 100},
+         {"name": "Ice Magic", "cost": 20, "damage": 80},
+         {"name": "Lightning Magic", "cost": 5, "damage": 60}]
+player = Person(500, 65, 34, 40, magic)
 enemy = Person(1200, 65, 40, 25, magic)
 
 running = True
@@ -19,10 +19,41 @@ while running:
     if index == 0:
         damage = player.generate_damage()
         enemy.take_damage(damage)
-        print("You've dealt", damage, "points of damage", " Enemy HP:", enemy.get_hp())
+        print(Bcolors.OKGREEN + Bcolors.BOLD + "You've dealt", damage, "points of damage" + Bcolors.ENDC)
+    elif index == 1:
+        player.choose_magic()
+        player_choice_magic = int(input("Choose Magic:\n")) - 1
+        magic_damage = player.generate_magic_damage(player_choice_magic)
+        enemy.take_damage(magic_damage)
+        spell = player.get_spell_name(player_choice_magic)
+        cost = player.get_mp_spell_cost(player_choice_magic)
+        current_mp = player.get_mp()
+        print(Bcolors.OKGREEN + Bcolors.BOLD + "You've dealt", magic_damage, "points of  magical damage with",
+              spell + Bcolors.ENDC)
+
+        if cost > current_mp:
+            print(Bcolors.OKRED, "\nNot Enough magic points", Bcolors.ENDC)
+            print("-------------------")
+            print("Your HP:", player.get_hp(), "\n" "Your MP:", player.get_mp())
+            print("Enemy HP:", enemy.get_hp())
+            continue
+        player.deduct_mp(cost)
 
     enemy_choice = 1
 
     enemy_damage = enemy.generate_damage()
     player.take_damage(enemy_damage)
-    print("The Enemy has dealt", enemy_damage, "points of damage", " Your HP:", player.get_hp())
+    print(Bcolors.OKRED + Bcolors.BOLD + "The Enemy has dealt", enemy_damage, "points of damage." + Bcolors.ENDC)
+    print("-------------------")
+    print(Bcolors.OKGREEN + Bcolors.BOLD + "Your HP:", str(player.get_hp()),
+          "/", str(player.get_max_hp()) + Bcolors.ENDC)
+    print(Bcolors.OKGREEN + Bcolors.BOLD + "Your MP:", str(player.get_mp()),
+          "/", str(player.get_max_mp()) + Bcolors.ENDC)
+    print(Bcolors.OKGREEN + Bcolors.BOLD + "Enemy HP:", str(enemy.get_hp()) + Bcolors.ENDC)
+
+    if enemy.get_hp() == 0:
+        print(Bcolors.OKGREEN, "You've Won", Bcolors.ENDC)
+        running = False
+    elif player.get_hp() == 0:
+        print(Bcolors.OKRED, "You've been Defeated", Bcolors.ENDC)
+        running = False
